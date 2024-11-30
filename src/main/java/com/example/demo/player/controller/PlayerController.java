3c03cb7e68172b7212d118f0ca045ce91ffda506
silenc3502 <gcccompil3r@gmail.com>
2024-11-30 17:27:16 +0900

    feat: [이상훈] Player 생성 [SBwD-5]

--- src/main/java/com/example/demo/player/controller/PlayerController.java
+++ src/main/java/com/example/demo/player/controller/PlayerController.java
@@ -1,35 +1,35 @@
-//package com.example.demo.player.controller;
-//
-//import com.example.demo.player.controller.request_form.PlayerCreateRequestForm;
-//import com.example.demo.player.controller.response_form.PlayerCreateResponseForm;
-//import com.example.demo.player.controller.response_form.PlayerListResponseForm;
-//import com.example.demo.player.entity.Player;
-//import com.example.demo.player.service.response.PlayerCreateResponse;
-//import com.example.demo.player.service.response.PlayerListResponse;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.*;
-//
-//import com.example.demo.player.service.PlayerService;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Slf4j
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/player")
-//public class PlayerController {
-//    final private PlayerService playerService;
-//
-//    @GetMapping("/create")
-//    public PlayerCreateResponseForm createPlayer(@ModelAttribute PlayerCreateRequestForm playerCreateRequestForm) {
-//        log.info("createPlayer() called!");
-//
-//        PlayerCreateResponse response = playerService.createPlayer(playerCreateRequestForm.toPlayerCreateRequest());
-//        return PlayerCreateResponseForm.from(response);
-//    }
-//
+package com.example.demo.player.controller;
+
+import com.example.demo.player.controller.request_form.PlayerCreateRequestForm;
+import com.example.demo.player.controller.response_form.PlayerCreateResponseForm;
+import com.example.demo.player.controller.response_form.PlayerListResponseForm;
+import com.example.demo.player.entity.Player;
+import com.example.demo.player.service.response.PlayerCreateResponse;
+import com.example.demo.player.service.response.PlayerListResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.player.service.PlayerService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/player")
+public class PlayerController {
+    final private PlayerService playerService;
+
+    @GetMapping("/create")
+    public PlayerCreateResponseForm createPlayer(@ModelAttribute PlayerCreateRequestForm playerCreateRequestForm) {
+        log.info("createPlayer() called!");
+
+        PlayerCreateResponse response = playerService.createPlayer(playerCreateRequestForm.toPlayerCreateRequest());
+        return PlayerCreateResponseForm.from(response);
+    }
+
 //    @GetMapping("/list")
 //    public List<PlayerListResponseForm> listPlayer() {
 //        log.info("listPlayer() called!");
@@ -40,4 +40,4 @@
 //                .map(PlayerListResponseForm::from)
 //                .collect(Collectors.toList());
 //    }
-//}
+}
--- src/main/java/com/example/demo/player/entity/Player.java
+++ src/main/java/com/example/demo/player/entity/Player.java
@@ -1,25 +1,26 @@
 package com.example.demo.player.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
+@Entity
 public class Player {
-    private static int idCounter = 0;
-    private final int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     private String nickname;
-    private List<Integer> diceIdList;
 
     public Player(String nickname) {
-        this.id = ++idCounter;
         this.nickname = nickname;
-        this.diceIdList = new ArrayList<>();
     }
 
-    public void addDiceId(int diceId) {
-        this.diceIdList.add(diceId);
-    }
+    public Player() { }
 }
